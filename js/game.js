b946f2ee1e379a4f86dccc0f0a518815f887bf3d
João Silvestre <jsvtop@gmail.com>
2020-05-23 22:34:21 +0100

    dano boss mapa gotico

--- js/game.js
+++ js/game.js
@@ -17,12 +17,12 @@ class Game extends Phaser.Game{
         //this.scene.start('Forest');
 
         //Mapa Castle
-        this.scene.add('Castle',Castle);
-        this.scene.start('Castle');
+        //this.scene.add('Castle',Castle);
+        //this.scene.start('Castle');
 
         //Mapa Ghost-Horror
-        //this.scene.add('Gothic-Horror',Ghotic);
-        //this.scene.start('Gothic-Horror');
+        this.scene.add('Gothic-Horror',Ghotic);
+        this.scene.start('Gothic-Horror');
 
     }
 }
