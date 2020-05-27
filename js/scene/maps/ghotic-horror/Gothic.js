80d4ef8e72175ad892c8ba23bf17b59954dee821
PedroPinheiro99 <49284620+PedroPinheiro99@users.noreply.github.com>
2020-05-27 13:44:10 +0100

    AGORA SIM VERSAO FINAL

--- js/scene/maps/ghotic-horror/Gothic.js
+++ js/scene/maps/ghotic-horror/Gothic.js
@@ -474,13 +474,13 @@ export default class Gothic extends Phaser.Scene {
               upgrades[0] = this.potion_hp.coins;
               this.potion_hp.price_hp.setText('x'+this.potion_hp.coins) // atualiza o preco
             }else if(Phaser.Input.Keyboard.JustDown(this.press2) && coins >= this.potion_velocity.coins){
-              this.archer.velocity += 300;
+              this.archer.velocity += 30;
               coins -= this.potion_velocity.coins;
               this.potion_velocity.coins *= 2; // para o preco dos upgrades aumentar sempre que se compra 
               upgrades[1] = this.potion_velocity.coins;
               this.potion_hp.price_hp1.setText('x'+this.potion_velocity.coins) // atualiza o preco
             }else if(Phaser.Input.Keyboard.JustDown(this.press3) && coins >= this.potion_damage.coins){
-              this.archer.archerDamage += 1500; 
+              this.archer.archerDamage += 15;
               if(this.archer.archerDamage > 55){
                 this.archer.archerDamage = 100;
               }
